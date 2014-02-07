f5286fcde6f43a440b857cceb5c7cba12b1ebd14
motorolja <rasmusholm957@hotmail.com>
2014-02-07 01:21:44 +0100

    funkar ok, dock buggit

--- TDTS04/lab2.java
+++ TDTS04/lab2.java
@@ -27,45 +27,68 @@ public class lab2
     public static void Server( Socket server_side ) throws Exception
     {
 	/****** server-side ********************/
+	BufferedReader from_client = new BufferedReader( new InputStreamReader( server_side.getInputStream() ) ); // gets the input and reads it to a buffer
+	PrintStream to_client = new PrintStream( server_side.getOutputStream() ); // enables output
 
-	PrintWriter to_client = new PrintWriter(server_side.getOutputStream(), true); // enables output
-	BufferedReader from_client = new BufferedReader( new InputStreamReader ( server_side.getInputStream() ) ); // gets the input and reads it to a buffer
-
-	String hostname = "liu.se";
+	String hostname, input_line = from_client.readLine(), second = from_client.readLine();
+	hostname = second.substring(6);
 	System.out.println("Hostname: " + hostname); // troubleshooting
 
 	/*-----------------------------------*/
 	/****** client-side ******************/
 			
 	Socket client_side = new Socket(hostname, 80); // initiates with the hostname and port
-	PrintWriter to_server = new PrintWriter( client_side.getOutputStream(), true ); // enables output 
-	BufferedReader from_server = new BufferedReader( new InputStreamReader ( client_side.getInputStream() ) ); // gets the input and reads it to a buffer
+	BufferedReader from_server = new BufferedReader( new InputStreamReader( client_side.getInputStream() ) ); // gets the input and reads it to a buffer
+	PrintStream to_server = new PrintStream( client_side.getOutputStream() ); // enables output 
 	
 	System.out.println( "before write to server" );
-	String input_line = null;
-	for( int i = 0; i < 8; ++i)
+	to_server.print(input_line + "\r\n");
+	to_server.print(second + "\r\n");
+       
+	while( true )
 	    {
 		input_line = from_client.readLine();
-		if( input_line == null )
+		if( input_line != null && input_line.isEmpty())
 		    break;
-		System.out.println(input_line);
-		to_server.println(input_line);
+		else
+		    {
+			if( input_line.startsWith("Connection:") )
+			    input_line = "Connection: close";
+			to_server.print(input_line + "\r\n");
+			System.out.println(input_line);
+		    }
 	    }
-	to_server.println("\r");
+	to_server.print("\r\n");
 	/*-----------------------------------*/	
 	/****** server-side ********************/
 	System.out.println( "before write to client");
 	input_line = null;
+	boolean headers = true;
 	while( true )
 	    {
 		input_line = from_server.readLine();
-		if( input_line == null )
-		    break;
-		System.out.println(input_line);	
-		to_client.println(input_line);
+		if( input_line != null && input_line.isEmpty() )
+		    {
+			headers = false;
+			to_client.print("\n");
+		    }
+		else if( input_line == null )
+		   break;
+		else
+		    {
+			if( headers )
+			    to_client.print(input_line + "\r\n");
+			else
+			    to_client.print(input_line + "\n");
+		    }
 	    }
 	/*-----------------------------------*/
 	System.out.println("End of transmission!");
+	to_server.close();
+	to_client.close();
+	client_side.close();
+	server_side.close();
+	
     }
     
 }
