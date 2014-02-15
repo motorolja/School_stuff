cae331fd6c1ce0e8881027056fc9abe3140f2049
motorolja <rasmusholm957@hotmail.com>
2014-02-15 23:20:32 +0100

    made a proxy with threads

--- TDTS04/proxy with threads/client.java
+++ TDTS04/proxy with threads/client.java
@@ -6,10 +6,11 @@ public class client extends Thread
     private Socket client_side = null;
     private PrintWriter out = null;
     private InputStream in = null;
-    
+    private boolean is_html = false;    
+
     public client( String host, int port) throws IOException
     {
-        super("client"); 
+        super("client");
 	System.out.println("in client constructor");
 	client_side = new Socket(host, port);
 	client_side.setKeepAlive(false); // Connection: close
@@ -20,15 +21,20 @@ public class client extends Thread
     
     public void send_line( String input_line )
     {
-	if( input_line.toLowerCase().startsWith("connection:") )
-	    input_line = "Connection: close";
 	
 	if ( input_line.toLowerCase().startsWith("accept-encoding:") )
 	    return;
 
-	out.print(input_line + "\r\n");
-	System.out.println(input_line);
+	else if( input_line.toLowerCase().startsWith("connection:") )
+	    input_line = "Connection: close";
 
+	else if( input_line.toLowerCase().startsWith("accept" ) && input_line.contains("html") )
+	    {
+		System.out.println("is html");
+		is_html = true;
+	    }
+	System.out.println(input_line);
+	out.print(input_line + "\r\n");
     }
     public void send_end()
     {
@@ -38,25 +44,42 @@ public class client extends Thread
 
     public int read( byte[] buffer ) throws IOException
     {
-	System.out.println("reading");
+	//System.out.println("reading");
 	in.available();
 	return in.read(buffer);
-    } 
+    }
+
+    public void clear_buffer() throws IOException
+    {
+	in.skip( Integer.MAX_VALUE );
+    }
+
+    public void new_host( String host ) throws IOException
+    {
+	close();
+	client_side = new Socket( host, 80 );
+	out = new PrintWriter( client_side.getOutputStream(), true ); // true = autoflush
+	in = client_side.getInputStream() ;	
+    }
 
     public void close()
     {
-	try 
-	    { 
+	try
+	    {
 		client_side.shutdownInput();
 		client_side.shutdownOutput();
 		in.close();
 		out.close();
-		client_side.close(); 
+		client_side.close();
 	    }
 	catch( IOException e )
 	    {
 		System.err.println("Error while closing client_side socket");
 		e.printStackTrace();
 	    }
+    }
+    public boolean get_is_html()
+    {
+	return is_html;
     }
 }
--- TDTS04/proxy with threads/connection.java
+++ TDTS04/proxy with threads/connection.java
@@ -1,29 +1,73 @@
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
 
-public class connection extends Thread 
+public class connection extends Thread
 {
     private Socket server_side = null;
     private OutputStream out = null;
     private BufferedReader in = null;
-
+    private ArrayList<String> bad_words = new ArrayList<String>();
+    private BufferedReader byte_reader = null;
+    
     public connection( Socket server_side ) throws SocketException
     {
-        super("connection"); 
+        super("connection");
 	this.server_side = server_side;
 	this.server_side.setKeepAlive(true);
+	System.out.println("created a new connection");
     }
     
-    public void run() 
+    public void add_bad_words( String w )
     {
+	bad_words.add( w.toLowerCase() );
+    }
+    
+    public boolean check_content (byte[] buffer ) throws IOException
+    {
+	String input_line = null;
+	InputStream is = new ByteArrayInputStream( buffer );
+	byte_reader = new BufferedReader( new InputStreamReader( is ,"UTF-8") );
+
+	while( true )
+	    {
+
+		input_line = byte_reader.readLine();
+		if( input_line == null || input_line.length() == 0)
+		    {
+			break;
+		    }
+		else if( bad_word( input_line ) )
+		    {
+			return true;
+		    }
+	    }
+	return false;
+	
+    }
+    public void run()
+    {
+
+	//ADD/REMOVE FORBIDDEN WORDS HERE
+	/**********************************/
+	//EXEMPEL : add_bad_words( "Norrköpping" );
+	//Norrköping is now a forbidden word!
+	add_bad_words("Batman");
+
+
+	/***********************************/
 	System.out.println("START 1-- after a request on welcome-socket");
 	try
 	    {
-		out =  server_side.getOutputStream();
-		in =  new BufferedReader( new InputStreamReader( server_side.getInputStream() ) );
+		out = server_side.getOutputStream();
+		in = new BufferedReader( new InputStreamReader( server_side.getInputStream() ) );
 
 		// a class that handles the client_side of the proxy
 		String host = get_host(in);
+
+		if( host.equals("-1") )
+		    return;
+		    
 		System.out.println("******hostname is: " + host);
 		client client_side = new client( host, 80 );
 
@@ -34,39 +78,43 @@ public class connection extends Thread
 		    {
 			in.ready();
 			input_line = in.readLine();
-			if( input_line == null  || input_line.length() == 0)
+			if( input_line == null || input_line.length() == 0)
 			    {
 				client_side.send_end();
 				System.out.println("End of request!");
 				break;
 			    }
-			System.out.println(input_line);
 			client_side.send_line(input_line);
 		    }
 
-		
+
 		// fetch the input from server_side to client_side
+		boolean valid_content = true;
 		byte[] buffer = new byte[1024];
 		int counter = 0;
 		while( ( counter = client_side.read(buffer) ) != -1 )
-		       {
-			   System.out.println("yo");
-			   System.out.println( new String(buffer) );
-			   send_data(buffer,counter);
-		       }
-		System.out.println("never in?");
-		boolean valid_content = true;
-		 
+		    {
+			//System.out.println( new String(buffer) );
+			if( client_side.get_is_html() && check_content( buffer) )
+			    {
+				System.out.println("found bad word");
+				valid_content = false;
+				break;
+			    }
+		
+			send_data(buffer,counter); 
+		    }
+
 		// if we have invalid content we send our predefined page
 		if( !valid_content )
-		    invalid_content();
+		    invalid_content( client_side );
 		// closes the client_side socket
 		client_side.close();
 		out.flush();
 
 		System.out.println(" END ---Before we closed the server_side socket");
 		// after we are done the connection is closed
-		server_side.close(); 
+		server_side.close();
 	    }
 	catch( IOException e )
 	    {
@@ -79,23 +127,27 @@ public class connection extends Thread
     {
 	System.out.println("in get hostname");
 	// sets a mark of where to reset the buffer
-	in.mark(1024); 
-	boolean faulty_host = false;
-	String first_line = in.readLine(); 
+	in.mark(1024);
+	in.ready();
+	String first_line = in.readLine();
 	// some url check
-	
+	if( bad_url(first_line) )
+	    {
+		return invalid_url();
+	    }
+
 	String host_line = in.readLine();
-	if( !host_line.isEmpty() && host_line.length() < 7)
-	    faulty_host = true;
 
 	System.out.println("-----after hostname loop");
 	// resets the buffer
 	in.reset();
-	// incase we miss the host data
-	if( faulty_host )
-	    return "";
 	// "host: hostname...." [0,1,2,3,4,5,6,..,n] hostname starts at index 6 of the second_line
 	return host_line.substring(6);
+    }
+
+    public void clear_buffer() throws IOException
+    {
+	in.skip( Integer.MAX_VALUE );
     }
 
     public void send_data( byte[] buffer, int counter ) throws IOException
@@ -103,13 +155,79 @@ public class connection extends Thread
 	out.write(buffer,0, counter );
     }
 
-    public void invalid_content()
+    public boolean bad_url( String first_line )
     {
+	return bad_word( first_line );
+    }
+
+    public String invalid_url() throws IOException
+    {
+	System.out.println("Invalid URL");
+	client client_side = new client("www.ida.liu.se", 80);
+
+	client_side.send_line("GET http://www.ida.liu.se/~TDTS04/labs/2011/ass2/error1.html /HTTP/1.1");
+	client_side.send_line("host: www.ida.liu.se");
+	client_side.send_line("Connection: close");
+	client_side.send_end();
+
+	byte[] buffer = new byte[1024];
+	int counter = 0;
+
+	try {
+	    while( ( counter = client_side.read(buffer) ) != -1 )
+		{
+		    //System.out.println( new String(buffer) );
+		    send_data(buffer,counter);
+		}
+	}
+	catch( IOException e )
+	    {
+		System.err.println( "Error in connection");
+		e.printStackTrace();
+	    }
 	
+	client_side.close();
+	return "-1";
+    }
+    public void invalid_content( client client_side) throws IOException
+    {
+	System.out.println("Invalid content");
+ 	client_side.clear_buffer();
+
+	client_side.new_host( "www.ida.liu.se");
+
+	client_side.send_line("GET http://www.ida.liu.se/~TDTS04/labs/2011/ass2/error2.html /HTTP/1.1");
+	client_side.send_line("host: www.ida.liu.se");
+	client_side.send_line("Connection: close");
+	client_side.send_end();
+
+	byte[] buffer = new byte[1024];
+	int counter = 0;
+
+	try {
+	    while( ( counter = client_side.read(buffer) ) != -1 )
+		{
+		    send_data(buffer,counter);
+		}
+	}
+	catch( IOException e )
+	    {
+		System.err.println( "Error in connection");
+		e.printStackTrace();
+	    }		
     }
 
     public boolean bad_word(String input_line)
     {
+	System.out.println("in bad word");
+	if( input_line.length() != 0 )
+	    {
+		for( int i = 0; i < bad_words.size(); ++i )
+		    {
+			if( input_line.toLowerCase().indexOf( bad_words.get(i)  ) >= 0 )
+			    return true;
+		    }
+	    }
 	return false;
     }
 }
