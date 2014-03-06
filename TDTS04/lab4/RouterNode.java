336389414c59fce66275b9ddc8170db370791265
motorolja <rasmusholm957@hotmail.com>
2014-03-06 16:33:59 +0100

    updated with poison reverse

--- TDTS04/lab4/RouterNode.java
+++ TDTS04/lab4/RouterNode.java
@@ -1,115 +1,229 @@
-import javax.swing.*;        
+ import javax.swing.*;        
 
-public class RouterNode {
-    private int myID;
-    private GuiTextArea myGUI;
-    private RouterSimulator sim;
-    private boolean poisoned_reverse;
-    // costs[0] = direct cost, costs[1] = shortest path, costs[2] = next Route for shortest path
-    private int[][] costs = new int[3][RouterSimulator.NUM_NODES];
-
+public class RouterNode 
+{
+    private int myID; //Router id
+    private GuiTextArea myGUI; //GUI 
+    private RouterSimulator sim; 
+    private boolean poison_reverse = true;
+    private int[] cost         = new int[RouterSimulator.NUM_NODES];
+    private int[] direct_cost  = new int[RouterSimulator.NUM_NODES];
+    private int[] route       = new  int[RouterSimulator.NUM_NODES];
+    private int[][] router_tables = new int[RouterSimulator.NUM_NODES][RouterSimulator.NUM_NODES];
     //--------------------------------------------------
-    public RouterNode(int ID, RouterSimulator sim, int[] costs) 
+    public RouterNode(int ID, RouterSimulator sim, int[] costs)
     {
 	myID = ID;
 	this.sim = sim;
 	myGUI =new GuiTextArea("  Output window for Router #"+ ID + "  ");
-	poisoned_reverse = false;
 
-	System.arraycopy(costs, 0, this.costs[0], 0, RouterSimulator.NUM_NODES);
-	this.costs[1] = this.costs[0];
+	System.arraycopy(costs, 0, this.cost, 0, RouterSimulator.NUM_NODES);
 
-	for( int i = 0; i < this.costs[2].length; ++i )
-	    this.costs[2][i] = i;
+	
+	for( int i = 0; i < cost.length; ++i )
+	    {
+		direct_cost[i] = costs[i]; // Fastes way is through neighbor
+		route[i] = i;
+	    }
 
-	// Send update to get simulation going
-	for( int i = 0; i < costs.length; ++i )
-	{
-	    sendUpdate( new RouterPacket( myID, i, costs ) );
-	}
+	for( int i = 0; i < cost.length; ++i )
+	    {
+		if( cost[i] != 999 )
+		    {
+			RouterPacket updated_table = new RouterPacket( myID, i, cost  );
+			sendUpdate( updated_table );
+		    }
+	    }
+	printDistanceTable();	
+
     }
 
     //--------------------------------------------------
     public void recvUpdate(RouterPacket pkt) 
     {
+	int pkt_mincost;
 	boolean changed_table = false;
-	for( int i = 0; i < pkt.mincost.length; ++i )
+
+	for( int i = 0; i < cost.length; ++i)
 	    {
-		// if we route through and the costs dont match or our current shortest path is longer
-		if( ( pkt.sourceid == costs[2][i] && costs[1][pkt.sourceid] != pkt.mincost[myID] + pkt.mincost[i] ) ||
-		    ( pkt.mincost[myID] + pkt.mincost[i] < costs[1][i] ) )
+		pkt_mincost = pkt.mincost[i];
+	
+       		if( (route[ i ] == pkt.sourceid && cost[ i ] != direct_cost[ pkt.sourceid ] + pkt_mincost)
+		    ||
+		    cost[ i ] > pkt_mincost + direct_cost[ pkt.sourceid ]   )  
 		    {
-			costs[1][i] = pkt.mincost[myID] + pkt.mincost[i];
-			// check if this is correct later
-			if( poisoned_reverse )
+			cost[ i ] =  direct_cost[ pkt.sourceid ] + pkt_mincost;
+			route[ i ] = pkt.sourceid; 
+			changed_table = true;
+		    }
+
+		//Update matrix
+		router_tables [ pkt.sourceid ] [ i ] = pkt_mincost;
+	    }
+
+	printDistanceTable();
+	if( changed_table  )
+	    {
+		for( int i = 0; i < cost.length; ++i )
+		    {
+			if(  cost[i] != 999 )
 			    {
-				sendUpdate(  new RouterPacket( myID, i, costs[1] ) );			    
-				printDistanceTable();
-			    }			
+				RouterPacket updated_table;
+				if( poison_reverse )
+				    {
+					int[] temp = new int[cost.length];
+
+					for( int y = 0; y < temp.length; ++y )
+					    {
+						if( route[y] == i   )
+						    {
+							temp[y] = 999;
+						    }
+						else 
+						    {
+							temp[y] = direct_cost[y];
+						    }
+					    }	
+					updated_table = new RouterPacket( myID, i, temp);
+				    }
+				else
+				    {
+					updated_table = new RouterPacket( myID, i, direct_cost);
+				    }
+				sendUpdate(  updated_table );
+			    }
+
+		    }
+	    }
+    }
+  
+
+    //--------------------------------------------------
+    private void sendUpdate(RouterPacket pkt) 
+    {
+	sim.toLayer2(pkt); //DONT TOUCH THIS
+    }
+  
+
+    //--------------------------------------------------
+    public void printDistanceTable() 
+    {
+	myGUI.println("Current table for " + myID +
+		      "  at time " + sim.getClocktime());
+
+	myGUI.print("Router: ");
+	for( int i = 0; i < cost.length; ++i )
+	    {
+		myGUI.print(  i + "  ");
+	    }
+	myGUI.print("\n");
+
+	myGUI.print("Cost  : ");
+	for( int i = 0; i < cost.length; ++i )
+	    {
+		myGUI.print( cost[i] + "  ");
+	    }
+	myGUI.print("\n");
+
+	myGUI.print("Route : ");
+	for( int i = 0; i < cost.length; ++i )
+	    {
+		if( cost[i] == 999 )
+		    {
+			myGUI.print( "-"  + "  ");
+		    }
+		else
+		    {
+			myGUI.print( route[i] + "  ");
+		    }
+	    }
+	myGUI.print("\n");
+    }
+
+    //--------------------------------------------------
+    public void updateLinkCost(int dest, int newcost)
+    {
+	direct_cost[dest] = newcost;
+	router_tables[myID] = direct_cost;
+
+	// For debugging!!!
+	myGUI.println("Call to updateLinkCost! see router_tables below\n");
+	for( int i = 0; i < cost.length; ++i)
+	    {
+		if( i == 0 )
+		    myGUI.print("Router\t" + i + "\t" );
+		else
+		    myGUI.print(i + "\t");
+	    }
+	myGUI.print("\n");
+
+	// For debugging!!!
+	for( int i = 0; i < router_tables.length; ++i )
+	    {
+		myGUI.print("nr: " + i + "\t");
+		for( int y = 0; y < router_tables[i].length; ++y )
+		    {
+			myGUI.print(router_tables[i][y] + "\t");
+		    }
+		myGUI.print("\n");
+	    }
+
+	// Need to calculate the new shortest path with our table
+	for( int i = 0; i < router_tables.length; ++i )
+	    {
+		// find all the routes which routes through the changed route
+		if( route[i] ==	dest )
+		    {
+			// resets our value to our direct cost
+			cost[i] = direct_cost[i];
+			// checks our table for a route to i
+			for( int y = 0; y < router_tables[i].length; ++y )
+			    {
+				// this is the case where we have only 1 node or have not recieved any packets from a node
+				if( router_tables[i].length < 2 || (router_tables[y][0] == 0 && router_tables[y][1] == 0) )
+				    {
+					break;
+				    }
+				// finds the current smallest cost
+				if( cost[i] > direct_cost[y] + router_tables[y][i] )
+				    {
+					cost[i] = direct_cost[y] + router_tables[y][i];
+					route[i] = y;
+				    }
+			    }
+		    }
+	    }
+
+	for( int i = 0; i < cost.length; ++i )
+	    {
+		if( cost[i] != 999 )
+		    {
+			RouterPacket updated_table;
+			if( poison_reverse )
+			    {
+				int[] temp  = new int[cost.length];
+				for( int j = 0; j < direct_cost.length; ++j )
+				    {
+					if( i == route[j] )
+					    {
+						temp[j] = 999;
+					    }
+					else
+					    {
+						temp[j] = direct_cost[j];
+					    }
+				    }
+				updated_table = new RouterPacket( myID, i, temp);
+			    }
 			else
 			    {
-				changed_table = true;
+				updated_table = new RouterPacket( myID, i, direct_cost);
 			    }
-		    } 
-	    }
-	if( changed_table )
-	    {
-		for( int i = 0; i < costs[1].length; ++i )
-		    sendUpdate(  new RouterPacket( myID, i, costs[1] ) );    
-		
-		printDistanceTable();
-	    }
-    }
-  
-  //--------------------------------------------------
-  private void sendUpdate(RouterPacket pkt) 
-    {
-	sim.toLayer2(pkt);
-    }
-  
+			sendUpdate(  updated_table );
+		    }
 
-  //--------------------------------------------------
-  public void printDistanceTable() 
-    {
-	// Timestamp
-	myGUI.println("Current table for " + myID +
-			"  at time " + sim.getClocktime());
-	// router numbers
-	myGUI.print("         ");
-	for( int i = 0; i < costs[0].length; ++i )
-	    myGUI.print(i + "\t");
-	myGUI.println("");
-	  
-	// cost table
-	String header = "Direct  :";
-	for(int i = 0; i < costs.length; ++i)
-	    {
-		if( i == 1 )
-		    header = "Shortest:";
-		else if( i == 2 )
-		    header = "Route   :";
-		myGUI.print( header );
-		
-		for( int e = 0; e < costs[i].length; ++e )
-		    myGUI.print( costs[i][e] + "\t" );
-		myGUI.println("");
 	    }
-    }
-
-  //--------------------------------------------------
-  public void updateLinkCost(int dest, int newcost) 
-    {
-	costs[0][dest] = newcost;
-	if( costs[2][dest] == dest )
-	    {
-		costs[1][dest] = newcost;
-	    }
-
-	for( int i = 0; i < costs[0].length; ++i )
-	    {
-		sendUpdate(  new RouterPacket( myID, i, costs[0] ) );    
-	    }		
-	printDistanceTable();	
     }
 
 }
+
